978317b51135fcb339205741c9c78e95b32b6a8e
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-10-06 19:59:15 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
   SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  OWNER_NUMBER: "254708583813", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
